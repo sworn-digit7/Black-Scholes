69977e4300a0613cdd0ca76788ea0930965fa4aa
Gurleen Singh <singhgurleen551@gmail.com>
2026-09-10 20:58:15 +0100

    put delta added

--- src/greeks.py
+++ src/greeks.py
@@ -3,8 +3,14 @@ from src.black_scholes import calculate_d1
 
 def call_delta(S, K, T, r, sigma):
 
-    delta = normal_cdf(calculate_d1(S, K, T, r, sigma))
+    delta_c = normal_cdf(calculate_d1(S, K, T, r, sigma))
 
-    return delta
+    return delta_c
+
+def put_delta(S, K, T, r, sigma):
+
+    delta_p = normal_cdf(calculate_d1(S, K, T, r, sigma)) - 1
+
+    return delta_p
 
 
