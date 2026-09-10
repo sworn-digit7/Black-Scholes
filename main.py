f94771020849703efb26de9b3e77173d261ac700
Gurleen Singh <singhgurleen551@gmail.com>
2026-09-10 19:31:17 +0100

    main

---
+++ main.py
@@ -0,0 +1,15 @@
+#      │   S = stock price
+# │     │   K = strike price
+# │     │   T = time to expiry
+# │     │   σ = volatility
+# │     │   r = risk-free rate
+
+
+
+def main():
+    ...
+
+
+
+if __name__ == "__main__":
+    main()
