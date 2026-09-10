9b75bef0923c6b081db56f647cd937fdd1dd7ac0
Gurleen Singh <singhgurleen551@gmail.com>
2026-09-10 19:48:27 +0100

    added d1 and d2 calcs

--- src/black_scholes.py
+++ src/black_scholes.py
@@ -11,3 +11,16 @@ def normal_cdf(x):
     cdf = 0.5 * (1 + math.erf(x / math.sqrt(2)))
 
     return cdf
+
+def calculate_d1(S, K, T, r, sigma):
+
+    d1 =  (math.log(S / K) + ((r + (sigma ** 2)) / 2) * T) / (sigma * math.sqrt(T))
+
+    return d1
+
+def calculate_d2(d1, T, sigma):
+
+    d2 = d1 - (sigma * math.sqrt(T))
+
+    return d2
+
