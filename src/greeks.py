2d7f25c915d3595fe4ab84d115dc7de424fc901d
Gurleen Singh <singhgurleen551@gmail.com>
2026-09-11 18:44:55 +0100

    gamma added

--- src/greeks.py
+++ src/greeks.py
@@ -1,5 +1,7 @@
 from src.black_scholes import normal_cdf
 from src.black_scholes import calculate_d1
+from src.black_scholes import normal_pdf
+import math
 
 def call_delta(S, K, T, r, sigma):
 
@@ -13,4 +15,9 @@ def put_delta(S, K, T, r, sigma):
 
     return delta_p
 
+def gamma(S, K, T, r, sigma):
+
+    r = normal_pdf(calculate_d1(S, K, T, r, sigma)) / ( S * sigma * math.sqrt(T))
+
+    return r
 
