33a59d591ba4720e40434f98d0c8ebedf981ced5
Gurleen Singh <singhgurleen551@gmail.com>
2026-09-12 12:00:06 +0100

    minor correction

--- src/plotting.py
+++ src/plotting.py
@@ -1,17 +1,14 @@
-import numpy as np
+import os
 import matplotlib.pyplot as plt
 from src.black_scholes import call_price, put_price
 
 def plot_sensitivity(param_name, param_range, S, K, T, r, sigma, save_path):
-    
     call_prices = []
     put_prices = []
 
     for value in param_range:
-        # Build a fresh copy of inputs each loop, overriding just the one being swept
         params = {"S": S, "K": K, "T": T, "r": r, "sigma": sigma}
         params[param_name] = value
-
         call_prices.append(call_price(**params))
         put_prices.append(put_price(**params))
 
@@ -23,5 +20,7 @@ def plot_sensitivity(param_name, param_range, S, K, T, r, sigma, save_path):
     plt.title(f"Option Price Sensitivity to {param_name}")
     plt.legend()
     plt.grid(True)
+
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
     plt.savefig(save_path)
     plt.close()
