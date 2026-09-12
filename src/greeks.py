c1cd6801e74a41ae98413daf48f3059ad7d2bedd
Gurleen Singh <singhgurleen551@gmail.com>
2026-09-12 10:44:58 +0100

    added call theta

--- src/greeks.py
+++ src/greeks.py
@@ -1,5 +1,6 @@
 from src.black_scholes import normal_cdf
 from src.black_scholes import calculate_d1
+from src.black_scholes import calculate_d2
 from src.black_scholes import normal_pdf
 import math
 
@@ -26,4 +27,17 @@ def vega(S, K, T, r, sigma):
     vega = S * normal_pdf(calculate_d1(S, K, T, r, sigma)) * (math.sqrt(T))
 
     return vega
+
+def call_theta(S, K, T, r, sigma):
+
+    call_theta = (
+
+    (- (S * normal_pdf(calculate_d1(S, K, T, r, sigma)) * sigma) / ( 2 * math.sqrt(T) ))
+
+    - (r * K * (math.e ** (-r*T)) * normal_cdf(calculate_d2(S, K, T, r, sigma)))
+
+    )
+
+    return call_theta
+
 
